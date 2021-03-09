ad25171cc0436d9b81ca54795d1a91f5140652f8
HEXING19 <37543492+HEXING19@users.noreply.github.com>
2021-03-09 14:16:23 +0800

    更新命令备注

--- device.ps1
+++ device.ps1
@@ -1,13 +1,14 @@
+#设置脚本执行方式
 Set-ExecutionPolicy RemoteSigned
-
+#切换目录
 Set-Location “C:\Users\lg\Desktop\DeviceManagement\”
-
+#导入命令
 Import-Module .\DeviceManagement.psd1
-
+#获取隐藏设备中名称带有Network的设备
 Get-Device -ControlOptions DIGCF_ALLCLASSES | Sort-Object -Property Name | Where-Object {($_.IsPresent -eq $false) -and ($_.Name -like “*Network*”) } | ft Name, DriverVersion, DriverProvider, IsPresent, HasProblem, InstanceId -AutoSize
-
+#将上述结果赋值给变量
 $hiddenHypVNics = Get-Device -ControlOptions DIGCF_ALLCLASSES | Sort-Object -Property Name | Where-Object {($_.IsPresent -eq $false) -and ($_.Name -like “*Network*”) }
-
+#切换目录
 Set-Location "C:\Users\lg\AppData\Roaming\DevCon"
-
+#批量删除隐藏设备
 ForEach ($hiddenNic In $hiddenHypVNics) { $deviceid = “@" + $hiddenNic.InstanceId; .\devcon.exe /r remove $deviceid }
